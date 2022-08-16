dd9c703707f12aae03f19935c3942232c95aadab
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-16 11:40:42 -0400

    class 117

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,7 +4,22 @@ export const useCounter = (initialValue = 10) => {
 
     const [counter, setCounter] = useState(initialValue)
 
+    const increment = ( value = 1 ) => {
+        setCounter(counter + value);
+    }
+
+    const decrement = ( value = 1 ) => {
+        setCounter(counter - value);
+    }
+
+    const reset = () => {
+        setCounter( initialValue );
+    }
+
     return {
         counter,
+        increment,
+        decrement,
+        reset,
     }
 }
