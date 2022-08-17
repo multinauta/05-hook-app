5ee3329232b63a22e558708f8c1d40a1b045f76c
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-17 13:02:15 -0400

    class 121

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,17 +1,27 @@
+import { useEffect, useState } from "react";
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({ x: 0, y:0});
+
   useEffect(() => {
-    console.log('message mounted');
+
+    const onMouseMove = ({ x, y}) => {
+      //const coords = { x, y };
+      setCoords({ x, y})
+    }
+
+    window.addEventListener( 'mousemove', onMouseMove)
   
     return () => {
-        console.log('message Unmounted')
+        window.removeEventListener( 'mousemove', onMouseMove)
     }
   }, [])
   
   return (
     <>
       <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
     </>
   )
 }
