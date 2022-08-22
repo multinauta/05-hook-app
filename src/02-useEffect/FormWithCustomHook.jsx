1957ea3e24f4dab7e8461979f6fb7a5763ddb955
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-22 12:15:31 -0400

    class 122

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { useForm } from "../hooks/useForm";
+import { Message } from "./Message";
+
+export const FormWithCustomHook = () => {
+
+    const { formState, onInputChange, username, email, password} = useForm({
+         username: '',
+         email: '',
+         password: ''
+    });
+
+    useEffect( () => {
+      //  console.log('useEffect called!');
+    }, [])
+
+    useEffect( () => {
+     // console.log('formState change');
+    }, [formState])
+
+    useEffect( () => {
+     //   console.log('email change');
+      }, [email])
+
+  return (
+    <>
+        <h1>Simple Con CustomHook</h1>
+        <hr />
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value= { username }
+            onChange={ onInputChange }
+        />
+        <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="alvaro@email.com"
+            name="email"
+            value= { email }
+            onChange={ onInputChange }
+        />
+        <input
+            type="password"
+            className="form-control mt-2"
+            placeholder="Contraseña"
+            name="password"
+            value= { password }
+            onChange={ onInputChange }
+        />
+
+    </>
+  )
+}
