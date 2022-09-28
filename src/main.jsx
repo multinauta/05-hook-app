6408c35cf23893916cce6d9d0b96963bed7750cb
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-09-28 15:56:01 -0300

    class 128

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -10,6 +11,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>
 )
